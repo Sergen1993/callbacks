89323195718e7808ba3a45cde92744a60b1b8c9d
Sergen1993 <14488@coderacademy.edu.au>
2023-08-01 04:05:11 +1000

    Added npm packages

--- modules.js
+++ modules.js
@@ -1,3 +1,8 @@
-import math from './math.js'
+// import math from './math.js'
 
-console.log(math.add(5, 10))
+// console.log(math.add(5, 10))
+import Roll from 'roll'
+
+const roll = new Roll()
+const dice = roll.roll('3d10')
+console.log(dice)
